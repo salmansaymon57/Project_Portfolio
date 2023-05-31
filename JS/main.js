95fba0e50b5b5a1d22bbdb0bfd4371ef424e6351
Salman Saymon <salman.bhoot@gmail.com>
2023-05-31 21:45:13 +0600

    finished

---
+++ JS/main.js
@@ -0,0 +1,21 @@
+
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "0px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
+
+
+const mySidebar = document.getElementById('mySidebar');
+
+if (window.matchMedia("(max-width: 999px)").matches) {
+mySidebar.onclick = function(){
+    closeNav(); 
+  }
+}
+
+
